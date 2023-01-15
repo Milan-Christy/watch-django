5329ae297f8bcbf47f044ff0412e0db792cdd1c0
Milan-Christy <milaninja17@gmail.com>
2023-01-15 15:21:03 +0530

    handled cart and checkout page for loginned user

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from .models import Account
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
 # Register your models here.
+
 class AccountAdmin(BaseUserAdmin):
-    model        = Account
-    list_display = ('email', 'username', 'phone_number', 'date_joined', 'last_login', 'is_active', 'is_block')
+    #model        = Account
+    list_display      = ('email', 'username', 'phone_number', 'date_joined', 'last_login', 'is_active', 'is_block')
     filter_horizontal = ()
     list_filter       = ()
+    fieldsets = ()
     ordering          = ('-date_joined',)
-admin.site.register(Account,AccountAdmin ,)
+    readonly_fields = ('last_login', 'date_joined')
+admin.site.register(Account,AccountAdmin )
 
--- accounts/views.py
+++ accounts/views.py
@@ -12,6 +12,12 @@ from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
 from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
+
+from cart.views import _cart_id
+from cart.models import Cart, CartItem
+
+import requests
+
 # Create your views here.
 
 def register(request):
@@ -57,12 +63,68 @@ def login(request):
         user = auth.authenticate(email=email, password=password)
         
         if user is not None :
-            if not Account.objects.get(email = email).is_block:
+                try:
+                    
+                    cart                        = Cart.objects.get(cart_id = _cart_id(request))
+                    is_cart_item_exists         = CartItem.objects.filter(cart = cart).exists()
+                
+                    if is_cart_item_exists:
+                        cart_item               = CartItem.objects.filter(cart=cart)
+                        
+                        product_variation = []
+                        for item in cart_item:
+                            variation = item.variations.all()
+                            product_variation.append(list(variation))   
+                        
+                        #get the cartitem from user to access his production variations
+                        cart_item           = CartItem.objects.filter(user = user) #return cartitem objects
+          
+            
+                        ex_var_list = []
+                        id          = []
+                        for item in cart_item:
+                            existing_variation = item.variations.all()
+                            ex_var_list.append(list(existing_variation))
+                            id.append(item.id)
+                                    
+                        #product_variation = [1,2,3,4,5,6]
+                        #ex_var_list      = [4,6,3,5]
+                        
+                        for pr in product_variation:
+                            if pr in ex_var_list:
+                                index          = ex_var_list.index(pr)
+                                item_id        = id[index]
+                                item           = CartItem.objects.get(id =  item_id)
+                                item.quantity += 1
+                                item.user      = user
+                                item.save()
+                            else:
+                                cart_item = CartItem.objects.filter(cart = cart)
+                        
+                                for item in cart_item:
+                                    item.user  = user
+                                    item.save()        #assigning the user to cartitem
+                except:
+                    
+                    pass
                 auth.login(request, user)
-                #messages.success(request, 'You are now logged in.')
-                return redirect('home')
-            else:
-                messages.error(request, 'You are blocked')
+                messages.success(request, 'You are now logged in.')
+                url  =  request.META.get('HTTP_REFERER')
+                try:
+                    query = requests.utils.urlparse(url).query
+                    print('query ->', query)#next=/cart/checkout/
+                    print('--------------')
+                    
+                    params = dict(x.split('=') for x in query.split('&')) #it will split the  = sign and make next as key and cart/checkout as value
+                    # print('params ->', params)
+                    if 'next' in params:
+                        nextpage = params['next']
+                        return redirect(nextpage)
+                        
+                except:
+                    return redirect('dashboard')
+                    
+           
         else:
             messages.error(request, 'Invalid login credentials')
             return redirect('login')
@@ -104,7 +166,7 @@ def forgotPassword(request):
             
             #Reset Password email
             current_site  = get_current_site(request)
-            mail_subject  = 'Reset your Paswword'
+            mail_subject  = 'Reset your Password'
             message       = render_to_string('accounts/reset_password_email.html',{
                 'user'  : user,
                 'domain':current_site,
@@ -129,7 +191,7 @@ def resetpassword_validate(request, uidb64, token):
     except(TypeError,ValueError, OverflowError, Account.DoesNotExist):
         user = None
         
-    if user is not None and default_token_generator.check_token(user, token):
+    if user is not None and default_token_generator.check_token(user, token):#if the check token returns true i.e. if it has the token
         request.session['uid'] = uid
         messages.success(request, 'Please reset your password')
         return redirect('resetPassword')
@@ -145,7 +207,7 @@ def resetPassword(request):
         if password == confirm_password:
             uid  = request.session.get('uid')
             user = Account.objects.get(pk = uid)
-            user.set_password(password)
+            user.set_password(password)#this setpassword will take the password and hash it
             user.save()
             messages.success(request, 'Password reset successfull')
             return redirect('login')
